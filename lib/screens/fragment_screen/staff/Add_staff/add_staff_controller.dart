c230e8bcef0c25a50897672ae0b9525267dcd416
Indrajeet Raj <indrajeet62051@gmail.com>
2025-06-16 10:07:55 +0530

    controller

---
+++ lib/screens/fragment_screen/staff/Add_staff/add_staff_controller.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:image_picker/image_picker.dart';
+
+class add_staff_controller {
+  File ? uploadedImage;
+  final ImagePicker picker = ImagePicker();
+  final first_name = TextEditingController();
+  final last_Name = TextEditingController();
+  final email = TextEditingController();
+  final role = TextEditingController();
+  final department = TextEditingController();
+  List<bool> branch =[false,false,false];
+
+
+  Future<void> pickImage(ImageSource source) async {
+    final XFile? picked = await picker.pickImage(source: source);
+    if (picked != null) {
+      uploadedImage = File(picked.path);
+    }
+  }
+
+  void toggleBranch(int index) {
+    if (index >= 0 && index < branch.length) {
+      branch[index] = !branch[index];
+    }
+  }
+
+
+
+  void resetForm() {
+    first_name.clear();
+    last_Name.clear();
+    email.clear();
+    role.clear();
+    department.clear();
+    uploadedImage = null;
+    for (int i = 0; i < branch.length; i++) {
+      branch[i] = false;
+    }
+  }
+
+  void dispose() {
+    first_name.dispose();
+    last_Name.dispose();
+    email.dispose();
+    role.dispose();
+    department.dispose();
+  }
+
+}
